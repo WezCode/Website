86a859d7aae78f85e92b051dbe38ac5b39cb7128
User <wesley.rmit@gmail.com>
2020-09-17 13:29:07 +1000

    Updates

---
+++ precache-manifest.518d126b12ec334960ada02e0d6541aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f438baab44da3254790d4848612a662",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "96190cbc8adceee7b2da",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bc3cb354d351c9a8ca48",
+    "url": "/react-gh-pages/static/js/2.289cb601.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.289cb601.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96190cbc8adceee7b2da",
+    "url": "/react-gh-pages/static/js/main.1f34caef.chunk.js"
+  },
+  {
+    "revision": "e30818c4bc3a6a5567fc",
+    "url": "/react-gh-pages/static/js/runtime-main.afb67ab3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
